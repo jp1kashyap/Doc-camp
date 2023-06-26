4704e6382a5838f91a6d12fef68af43b5ec98820
JP <jp1kashyap@gmail.com>
2023-06-26 16:30:05 +0530

    printing

--- doctor/patient-list.php
+++ doctor/patient-list.php
@@ -1,14 +1,27 @@
 <?php include '../includes/header-main.php'; 
 include '../classes/Patient.php';
+include '../classes/Camp.php';
 include '../validation/DataValidator.php';
 $patient=new Patient();
+$camp=new Camp();
 $validate = new Data_Validator();
 $camp_id = isset($_REQUEST['doctor']) ? $_REQUEST['doctor'] : null;
 if(!$camp_id){
     echo("<script>location.href = '".BASE_URL."doctor/list.php';</script>");
 }
 $list=$patient->doctorPatients($camp_id);
+$doctor = $camp->edit($camp_id);
 ?>
+<style>
+    #toPrint #label{
+        position: absolute;
+        margin: 43px 10px;
+        background-color: #f6f8fa;
+        padding: 10px 12px;
+    }
+    .dataTable-wrapper{padding: 30px 11px;}
+    
+</style>
 <script src="<?=BASE_URL?>assets/js/simple-datatables.js"></script>
 <div >
     <ul class="flex space-x-2 rtl:space-x-reverse">
@@ -22,7 +35,29 @@ $list=$patient->doctorPatients($camp_id);
 
     <div x-data="exportTable">
     <div class="panel">
-        <table id="myTable" class="whitespace-nowrap table-hover"></table>
+    <div class="md:absolute md:top-5 ltr:md:left-5 rtl:md:right-5">
+            <div class="flex items-center flex-wrap mb-5">
+                <button type="button" class="btn btn-primary btn-sm m-1" @click="printTable">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ltr:mr-2 rtl:ml-2">
+                        <path d="M6 17.9827C4.44655 17.9359 3.51998 17.7626 2.87868 17.1213C2 16.2426 2 14.8284 2 12C2 9.17157 2 7.75736 2.87868 6.87868C3.75736 6 5.17157 6 8 6H16C18.8284 6 20.2426 6 21.1213 6.87868C22 7.75736 22 9.17157 22 12C22 14.8284 22 16.2426 21.1213 17.1213C20.48 17.7626 19.5535 17.9359 18 17.9827" stroke="currentColor" stroke-width="1.5" />
+                        <path opacity="0.5" d="M9 10H6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                        <path d="M19 14L5 14" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                        <path d="M18 14V16C18 18.8284 18 20.2426 17.1213 21.1213C16.2426 22 14.8284 22 12 22C9.17157 22 7.75736 22 6.87868 21.1213C6 20.2426 6 18.8284 6 16V14" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                        <path opacity="0.5" d="M17.9827 6C17.9359 4.44655 17.7626 3.51998 17.1213 2.87868C16.2427 2 14.8284 2 12 2C9.17158 2 7.75737 2 6.87869 2.87868C6.23739 3.51998 6.06414 4.44655 6.01733 6" stroke="currentColor" stroke-width="1.5" />
+                        <circle opacity="0.5" cx="17" cy="10" r="1" fill="currentColor" />
+                        <path opacity="0.5" d="M15 16.5H9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                        <path opacity="0.5" d="M13 19H9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                    </svg>
+                    PRINT
+                </button>
+            </div>
+        </div>
+        <div id="toPrint">
+        <div>
+            <label id="label">Doctor Name: <strong><?=isset($doctor) && $doctor['doctor']?$doctor['doctor']:"" ?></strong> Doctor Code: <strong><?=isset($doctor) && $doctor['doctor_code']?$doctor['doctor_code']:"" ?></strong> </label>
+        </div>
+        <table id="myTable" cellspacing="0" rules="all" border="1" class="whitespace-nowrap table-hover"></table>
+        </div>
     </div>
 </div>
 
@@ -33,7 +68,7 @@ $list=$patient->doctorPatients($camp_id);
             init() {
                 this.datatable = new simpleDatatables.DataTable('#myTable', {
                     data: {
-                        headings: ['Patient Name','Hospital','Age','Sex','Date','PSQi Score','psqi result'],
+                        headings: ['Patient Name','Hospital','Age','Sex','Date','PSQi Score','PSQi Result'],
                         data: <?=json_encode($list)?>
                     },
                     perPage: 10,
@@ -79,7 +114,48 @@ $list=$patient->doctorPatients($camp_id);
             },
 
             printTable() {
-                this.datatable.print();
+                //this.datatable.print();
+                var printWindow = window.open('');
+                printWindow.document.write('<html><head><title>Table Contents</title>');
+        
+                //Print the Table CSS.
+                printWindow.document.write('<style type = "text/css">');
+                printWindow.document.write(`body
+                    {
+                        font-family: Arial;
+                        font-size: 10pt;
+                    }
+                    table
+                    {
+                        border: 1px solid #ccc;
+                        border-collapse: collapse;
+                    }
+                    table th
+                    {
+                        background-color: #F7F7F7;
+                        color: #333;
+                        font-weight: bold;
+                    }
+                    table th, table td
+                    {
+                        padding: 5px;
+                        border: 1px solid #ccc;
+                    }
+                    label, .dataTable-wrapper{padding: 10px;}
+                    .dataTable-top, .dataTable-bottom{display:none;}
+                    `);
+                printWindow.document.write('</style>');
+                printWindow.document.write('</head>');
+        
+                //Print the DIV contents i.e. the HTML Table.
+                printWindow.document.write('<body>');
+                var divContents = document.getElementById("toPrint").outerHTML;
+                printWindow.document.write(divContents);
+                printWindow.document.write('</body>');
+                printWindow.document.write('</html>');
+                printWindow.document.close();
+                printWindow.print();
+                printWindow.close();
             },
 
             formatDate(date) {
--- patient/score.php
+++ patient/score.php
@@ -91,7 +91,7 @@ $question8Score=isset($questions[15]) && $questions[15]['score'] ? $questions[15
 $component7 = $question7Score + $question8Score;
 
 $psqiScore = $component1 + $component2 + $component3 + $component4 + $component5 + $component6 + $component7;
-$psqiResult = $psqiScore <= 5 ? 'Good Sleeper' : 'Some difficulties';
+$psqiResult = $psqiScore <= 5 ? 'Good Sleeper' : 'Severe difficulties';
 $patient->updateScore($patientId, $psqiScore, $psqiResult);
 ?>
 <link rel="stylesheet" href="<?=BASE_URL?>assets/css/flatpickr.min.css">
@@ -114,14 +114,14 @@ $patient->updateScore($patientId, $psqiScore, $psqiResult);
     <div class="pt-5">
 
         <div class="grid grid-cols-1 gap-6 pt-8 lg:grid-cols-1">
-            <div class="panel">
-                <div class=" text-center">
+            <div class="panel" id="toPrint">
+                <div class="text-center" style="text-align: center;">
                     <h1 style="font-size:24px;font-weight:700;" class="">The Pittsburgh Sleep Quality Index (PSQI)</h2>
                     <hr style="margin-top:20px"/>
                 </div>
                 <div class="mb-12" style="margin-top:20px">
                     <p>Instructions: The following questions relate to your usual sleep habits during the past month only. Your answers should indicate the</p>
-                    <table class=" table-bordered">
+                    <table class=" table-bordered" cellspacing="0" rules="all" border="1">
                         <tr>
                             <td>1. When have you usually gone to bed?</td>
                             <td colspan="4"><?=isset($questions[0])?$questions[0]['answer']:""?></td>
@@ -250,8 +250,70 @@ $patient->updateScore($patientId, $psqiScore, $psqiResult);
                         </tr>
                     </table>
                 </div>
+                <div class="flex items-center flex-wrap mb-5">
+                <button type="button" class="btn btn-primary btn-sm m-1" onclick="printTable()">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ltr:mr-2 rtl:ml-2">
+                        <path d="M6 17.9827C4.44655 17.9359 3.51998 17.7626 2.87868 17.1213C2 16.2426 2 14.8284 2 12C2 9.17157 2 7.75736 2.87868 6.87868C3.75736 6 5.17157 6 8 6H16C18.8284 6 20.2426 6 21.1213 6.87868C22 7.75736 22 9.17157 22 12C22 14.8284 22 16.2426 21.1213 17.1213C20.48 17.7626 19.5535 17.9359 18 17.9827" stroke="currentColor" stroke-width="1.5" />
+                        <path opacity="0.5" d="M9 10H6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                        <path d="M19 14L5 14" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                        <path d="M18 14V16C18 18.8284 18 20.2426 17.1213 21.1213C16.2426 22 14.8284 22 12 22C9.17157 22 7.75736 22 6.87868 21.1213C6 20.2426 6 18.8284 6 16V14" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                        <path opacity="0.5" d="M17.9827 6C17.9359 4.44655 17.7626 3.51998 17.1213 2.87868C16.2427 2 14.8284 2 12 2C9.17158 2 7.75737 2 6.87869 2.87868C6.23739 3.51998 6.06414 4.44655 6.01733 6" stroke="currentColor" stroke-width="1.5" />
+                        <circle opacity="0.5" cx="17" cy="10" r="1" fill="currentColor" />
+                        <path opacity="0.5" d="M15 16.5H9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                        <path opacity="0.5" d="M13 19H9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                    </svg>
+                    PRINT
+                </button>
             </div>
+            </div>
+            
         </div>
     </div>
 </div>
+<script>
+    function printTable() {
+        //this.datatable.print();
+        var printWindow = window.open('');
+        printWindow.document.write('<html><head><title>Table Contents</title>');
+
+        //Print the Table CSS.
+        printWindow.document.write('<style type = "text/css">');
+        printWindow.document.write(`body
+            {
+                font-family: Arial;
+                font-size: 10pt;
+            }
+            table
+            {
+                border: 1px solid #ccc;
+                border-collapse: collapse;
+            }
+            table th
+            {
+                background-color: #F7F7F7;
+                color: #333;
+                font-weight: bold;
+            }
+            table th, table td
+            {
+                padding: 5px;
+                border: 1px solid #ccc;
+            }
+            label, .dataTable-wrapper{padding: 10px;}
+            .dataTable-top, .dataTable-bottom, button{display:none;}
+            `);
+        printWindow.document.write('</style>');
+        printWindow.document.write('</head>');
+
+        //Print the DIV contents i.e. the HTML Table.
+        printWindow.document.write('<body>');
+        var divContents = document.getElementById("toPrint").outerHTML;
+        printWindow.document.write(divContents);
+        printWindow.document.write('</body>');
+        printWindow.document.write('</html>');
+        printWindow.document.close();
+        printWindow.print();
+        printWindow.close();
+    }
+</script>
 <?php include '../includes/footer-main.php'; ?>
